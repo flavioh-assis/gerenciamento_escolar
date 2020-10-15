660d0cb3014121c7b6ff771a5df4a66e656a201b
Flavio Assis <38504393+flaviohassis@users.noreply.github.com>
2020-10-15 15:37:23 -0300

    Criando a conexao com o BD

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -4,9 +4,9 @@ module.exports = {
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    database : 'ge_v1',
+    database : 'db_mvmi',
     user : 'postgres',
-    password : 'postgres'
+    password : 'postgres',
   },
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations')
--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -4,9 +4,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    database : 'ge_v1',
+    database : 'db_mvmi',
     user : 'postgres',
-    password : 'postgres'
+    password : 'postgres',
   }
   // connection: process.env.PG_CONNECTION_STRING,
   // searchPath: ['knex', 'public'],
