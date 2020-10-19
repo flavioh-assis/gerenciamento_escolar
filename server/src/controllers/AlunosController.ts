f01d09a57e8b73b58cd88b8ba6188ed3623611df
Flavio Assis <38504393+flaviohassis@users.noreply.github.com>
2020-10-19 16:25:14 -0300

    Feito o insert nas tabelas RM e Matriculas

--- server/src/controllers/AlunosController.ts
+++ server/src/controllers/AlunosController.ts
@@ -4,6 +4,10 @@ import db from '../database/connection'
 export default class AlunosController {
   async index(req: Request, res: Response) {
     const alunos = await db('tbAlunos')
+      .join('tbMatriculas', 'tbMatriculas.ra', '=', 'tbAlunos.ra')
+      .join('tbRm', 'tbRm.rm', '=', 'tbAlunos.rm')
+      .where('tbMatriculas.classe', 'ilike', '1º%')
+      // `%${bookDescription[0]}%`
 
     return res.json(alunos)
   }
@@ -12,7 +16,7 @@ export default class AlunosController {
     const {
       nome,
       ra,
-      rm,
+      // rm,
       nasc_cidade,
       nasc_uf,
       nacionalidade,
@@ -38,30 +42,61 @@ export default class AlunosController {
       proc_ano,
       ex_aluno,
       ano_desejado,
+      // tbMatriculas ----------------------
+      data_inicio,
+      data_fim,
+      ano_letivo,
+      classe,
+      status,
+      num_chamada,
+      idade,
     } = req.body
 
-    const day = nasc_data.substr(0, 2)
-    const month = nasc_data.substr(2, 2) - 1
-    const year = nasc_data.substr(4, 4)
+    let day = nasc_data.substr(0, 2)
+    let month = nasc_data.substr(2, 2) - 1
+    let year = nasc_data.substr(4, 4)
     const nasc_data_convert = new Date(year, month, day)
 
     let resp_validade_convert = null
 
     if (resp_validade != null) {
-      const day = resp_validade.substr(0, 2)
-      const month = resp_validade.substr(2, 2) - 1
-      const year = resp_validade.substr(4, 4)
+      day = resp_validade.substr(0, 2)
+      month = resp_validade.substr(2, 2) - 1
+      year = resp_validade.substr(4, 4)
 
-      resp_validade_convert = new Date(year, month, day)
+      let resp_validade_convert = new Date(year, month, day)
     }
+
+    // tbMatriculas --------------------------------
+    day = data_inicio.substr(0, 2)
+    month = data_inicio.substr(2, 2) - 1
+    year = data_inicio.substr(4, 4)
+    const data_inicio_convert = new Date(year, month, day)
+
+    let data_fim_convert = null
+
+    if (data_fim != null) {
+      day = data_fim.substr(0, 2)
+      month = data_fim.substr(2, 2) - 1
+      year = data_fim.substr(4, 4)
+      data_fim_convert = new Date(year, month, day)
+    }
+    // tbMatriculas --------------------------------
 
     const trx = await db.transaction()
 
     try {
+      const rm_gerado = await trx('tbRm').insert({
+        aluno: nome,
+        ano_letivo,
+        data_nasc: nasc_data_convert,
+        mae,
+      }).returning('rm')
+
       await trx('tbAlunos').insert({
         nome,
         ra,
-        rm,
+        rm: rm_gerado[0],
         nasc_cidade,
         nasc_uf,
         nacionalidade,
@@ -87,6 +122,17 @@ export default class AlunosController {
         proc_ano,
         ex_aluno,
         ano_desejado,
+      })
+
+      await trx('tbMatriculas').insert({
+        ra,
+        data_inicio: data_inicio_convert,
+        data_fim: data_fim_convert,
+        ano_letivo,
+        classe,
+        status,
+        num_chamada,
+        idade,
       })
 
       await trx.commit()
