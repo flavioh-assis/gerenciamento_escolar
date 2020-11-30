328e4a67496a210022cb92de32417fc94949d2d4
Flavio Assis <38504393+flaviohassis@users.noreply.github.com>
2020-11-30 16:38:18 -0300

    ok

--- web/src/components/BD/MostraClasses/index.tsx
+++ web/src/components/BD/MostraClasses/index.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { DataGrid, ColDef } from '@material-ui/data-grid'
+import api from '../../../services/api'
 
 import './styles.css'
 
-export default () => (
-  <div className="mostra-classes">
-    <p>Ano</p>
-    <p>Turma</p>
-    <p>Periodo</p>
-    <p>Sala</p>
-    <p>Professor</p>
-    <p>Ação</p>
-  </div>
-)
+const columns: ColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 /*, hide: true */ },
+  { field: 'ano', headerName: 'Ano', width: 90 },
+  { field: 'turma', headerName: 'Turma', width: 90 },
+  { field: 'periodo', headerName: 'Periodo', width: 90 },
+  { field: 'sala', headerName: 'Sala', width: 90 },
+  { field: 'professor', headerName: 'Professor', width: 170 }
+]
+
+export default () => {
+  const [classes, setClasses] = useState([])
+
+  useEffect(() => {
+    api.get('classes').then(response => {
+      setClasses(response.data)
+    })
+  }, [classes])
+
+  return (
+    <div className="mostra-classes">
+      <DataGrid rows={classes} columns={columns} pageSize={9} />
+    </div>
+  )
+}
