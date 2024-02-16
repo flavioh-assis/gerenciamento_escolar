79cb89b9fa24c88a6277fa1039b8557f28fb29e4
Flavio Assis <flavioh.dev@gmail.com>
2024-02-16 20:06:50 -0300

    build(front): add react-query

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Routes } from './routes';
 import './assets/styles/global.css';
 
-const App = () => <Routes />;
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <Routes />
+  </QueryClientProvider>
+);
 
 export { App };
